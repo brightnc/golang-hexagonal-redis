d6d3d681e5f8e8c28c959f1ed94f8ee121add884
Siwat Anandechsak <bright.siwat@hotmail.com>
2022-12-15 21:48:13 +0700

    add env

--- database/mariaDB.go
+++ database/mariaDB.go
@@ -1,12 +1,16 @@
 package database
 
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func InitDatabase() *gorm.DB {
-	dial := mysql.Open("root:43349690b@tcp(localhost:3306)/productsdb")
+	dsn := fmt.Sprintf("root:%v@tcp(localhost:%v)/productsdb", os.Getenv("PASSWORDDB"), os.Getenv("PORT"))
+	dial := mysql.Open(dsn)
 	db, err := gorm.Open(dial, &gorm.Config{})
 	if err != nil {
 		panic(err)
